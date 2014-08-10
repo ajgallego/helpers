501e3d4f1615b3e8735378e29f5227628ae2a812
Javier Gallego <ajgallego@gmail.com>
2014-08-10 11:17:05 +0200

    Add User Helper migrations

---
+++ src/migrations/1543_03_23_172956_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        // Creates the users table
+        Schema::create('users', function ($table) 
+        {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id')->unsigned();
+            $table->string('username')->unique();
+            $table->string('email')->unique()->index();
+            $table->string('password');
+            $table->string('confirmation_code');
+            $table->string('remember_token');
+            $table->boolean('confirmed')->default(false);
+            $table->timestamps();
+        });
+
+        // Creates password reminders table
+        Schema::create('password_reminders', function ($table) 
+        {
+            $table->engine = 'InnoDB';
+            
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::drop('password_reminders');
+        Schema::drop('users');
+    }
+}
