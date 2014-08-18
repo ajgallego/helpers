e52fe6ba36588b70e629230cab9044374278d3cb
Javier Gallego <ajgallego@gmail.com>
2014-08-18 17:16:49 +0200

    Add user translations and email views

---
+++ src/views/emails/auth/password_reset.blade.php
@@ -0,0 +1,10 @@
+<h1>{{ Lang::get('helpers::auth.email.password_reset.subject') }}</h1>
+
+<p>{{ Lang::get('helpers::auth.email.password_reset.greetings', array( 'name' => $user['username'])) }},</p>
+
+<p>{{ Lang::get('helpers::auth.email.password_reset.body') }}</p>
+<a href='{{ URL::to('user/reset_password/'.$token) }}'>
+    {{ URL::to('user/reset_password/'.$token)  }}
+</a>
+
+<p>{{ Lang::get('helpers::auth.email.password_reset.farewell') }}</p>
